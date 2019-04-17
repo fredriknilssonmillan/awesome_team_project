3b83675d05526d65515c50f4e355cb2982396e6d
sergioardz79 <sergioardz79@gmail.com>
2019-04-17 12:38:40 -0500

    including all locations plus other changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,11 +18,15 @@ var queryURLGeoIP = "https://geo.ipify.org/api/v1?" + apiKeyIP + "&ipAddress=" +
 console.log("queryURLGeoIP: " + queryURLGeoIP);
 // -------------------------------------------------------------------------------------------------------------
 // Code Snippet for CURRENT LOCATION logic
-// AJAX Function to call the current public IP, and the a second AJAX Function within to call the reverse code from the current IP
 
-$("#current").on("click", currentIPLocation);
+// Calling currentIPLocation function for current location button, with this syntaxis one can add/include more functions to the listening event
+$("#current").on("click", function(event){
+    event.preventDefault();
+    currentIPLocation();
+});
 
 
+// Function to get current location based on IP address, 1st ajax call gets IP, 2nd one reverse code the IP to get geolocation values
 function currentIPLocation() {
     $.ajax({
         url: queryIP, method: "GET"
@@ -62,8 +66,103 @@ var queryURLrandom = baseURL + randomBeer + apikey;
 console.log(queryURLrandom);
 var locations = [];
 
-$("#random").on("click", random);
 
+// Arrays for drop down menus values
+var allLocationsIds = [];
+var allLocationsNames = [];
+var allLocationsBreweriesNames = [];
+var allLocationsLocalities = [];
+var allLocationsRegions = [];
+var allLocationsCountries = [];
+
+var auxLocations = "locations";
+console.log("URL to get ALL Locations: " + baseURL + auxLocations + apikey);
+var queryLocationsURL = baseURL + auxLocations + apikey;
+
+// Calling getLocations function for another location button, with this syntaxis one can add/include more functions to the listening event
+$("#another").on("click", function(event){
+    event.preventDefault();
+    getLocations()
+});
+
+
+// Function to get all locations in order to create the drop down menus to choose a beer based on a location as first filter
+function getLocations() {
+    $.ajax({
+        url: queryLocationsURL, method: "GET"
+    })
+        .done(function (response) {
+            console.log("Total Locations available: " + response.data.length);
+            console.log(response.data);
+            var newRowTitles = $("<tr>").append(
+                $("<th>").text("Location ID"),
+                $("<th>").text("Location Name"),
+                $("<th>").text("Brewery Name"),
+                $("<th>").text("Locality"),
+                $("<th>").text("Region"),
+                $("<th>").text("Country"),
+                $("<th>").text("Latitude"),
+                $("<th>").text("Longitude"),
+            )
+            $("#resultscontainer").append(newRowTitles);
+            for (i = 0; i < response.data.length; i++) {
+                var newRow = $("<tr>").append(
+                    $("<td>").text(response.data[i].id),
+                    $("<td>").text(response.data[i].name),
+                    $("<td>").text(response.data[i].brewery.nameShortDisplay),
+                    $("<td>").text(response.data[i].locality),
+                    $("<td>").text(response.data[i].region),
+                    $("<td>").text(response.data[i].country.isoCode),
+                    $("<td>").text(response.data[i].latitude),
+                    $("<td>").text(response.data[i].longitude),
+                )
+                $("#resultscontainer").append(newRow);
+                // Series of If's to prevent duplicating values on the different arrays
+                if (allLocationsIds.includes(response.data[i].id) === false) {
+                    allLocationsIds.push(response.data[i].id);
+                }
+                if (allLocationsNames.includes(response.data[i].name) === false) {
+                    allLocationsNames.push(response.data[i].name);
+                }
+                if (allLocationsBreweriesNames.includes(response.data[i].brewery.nameShortDisplay) === false) {
+                    allLocationsBreweriesNames.push(response.data[i].brewery.nameShortDisplay);
+                }
+                if (allLocationsLocalities.includes(response.data[i].locality) === false) {
+                    allLocationsLocalities.push(response.data[i].locality);
+                }
+                if (allLocationsRegions.includes(response.data[i].region) === false) {
+                    allLocationsRegions.push(response.data[i].region);
+                }
+                if (allLocationsCountries.includes(response.data[i].country.isoCode) === false) {
+                    allLocationsCountries.push(response.data[i].country.isoCode);
+                }
+            }
+            console.log("Total Location IDs: " + allLocationsIds.length);
+            console.log(allLocationsIds);
+            console.log("Total Location Names: " + allLocationsNames.length);
+            console.log(allLocationsNames);
+            console.log("Total Breweries: " + allLocationsBreweriesNames.length);
+            console.log(allLocationsBreweriesNames);
+            console.log("Total Localities: " + allLocationsLocalities.length);
+            console.log(allLocationsLocalities);
+            console.log("Total Regions: " + allLocationsRegions.length);
+            console.log(allLocationsRegions);
+            console.log("Total Countries: " + allLocationsCountries.length);
+            console.log(allLocationsCountries);
+            }
+        )}
+
+// Calling random function for random location button, with this syntaxis one can add/include more functions to the listening event
+$("#random").on("click", function(event){
+    event.preventDefault();
+    random();
+});
+
+
+// Complete random function to get a random beer directly from the original random query (1st ajax call on function) in the BreweryDB API
+// Once there is a random beer selected, a 2nd ajax call is used to get the specifics about that beer
+// Furthermore there are a 3rd, 4th and 5th ajax calls, at the time the function was created, I (Sergio) was not aware that this was not needed,
+// NEED TO CORRECT THIS IN THE NEXT DAYS.... In order to simplify code and reduce the total amount of requests
 function random() {
     $.ajax({
         url: queryURLrandom, method: "GET"
@@ -76,6 +175,7 @@ function random() {
             console.log("Style: " + response.data.style.name);
             console.log("Style description: " + response.data.style.description);
             var beerID = response.data.id;
+            // This is the extra fields that makes it possible to get rid of the 3rd, 4th and 5th ajax calls
             var auxiliaryFields = "&withBreweries=Y&withSocialAccounts=Y&withIngredients=Y";
             var queryURLbeerByID = baseURL + "beer/" + beerID + apikey + auxiliaryFields;
             console.log(queryURLbeerByID);
@@ -83,10 +183,11 @@ function random() {
                 url: queryURLbeerByID, method: "GET"
             })
                 .done(function (response) {
+                    // At this point we need to improve the way of displaying results on the html
                     var selectedBeer = $("<p>").text("Selected Beer: " + response.data.name);
                     $("#resultscontainer").append(selectedBeer);
                     if (response.data.hasOwnProperty("labels")) {
-                        $("#resultscontainer").append("<img src=" + response.data.labels.large + ">");
+                        $("#resultscontainer").append("<img src=" + response.data.labels.medium + ">");
                     }
                 })
             console.log("Beer ID: " + beerID);
@@ -109,11 +210,12 @@ function random() {
                         .done(function (response) {
                             console.log(response.data.length);
                             for (i = 0; i < response.data.length; i++) {
+                                // This is to create a list of all potential locations from a beer/brewery
                                 locations.push(response.data[i].id);
-                                // console.log(response.data[i].id);
                             }
                             console.log(locations);
                             console.log(locations.length);
+                            // To randomly select a location if there is more than 1
                             var randomIndex = Math.floor(Math.random() * locations.length);
                             console.log(randomIndex);
                             var winnerID = locations[randomIndex];
@@ -137,6 +239,8 @@ function random() {
 }
 
 $("#resultscontainer").hide();
+
+
 
 $(".action").click(function () {
 
